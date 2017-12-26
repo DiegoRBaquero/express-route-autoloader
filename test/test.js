87c542a12dab204f8bd7fab9dc9eedc0dc4c4a65
Diego Rodríguez <diegorbaquero@gmail.com>
2017-12-26 16:43:00 -0500

    Add tests

--- test/test.js
+++ test/test.js
@@ -1,10 +1,29 @@
 import test from 'ava'
+const request = require('supertest')
 
-test('Route is loaded', async t => {
+let server = null
+
+process.chdir('./example')
+
+test.before(t => {
+  server = require('../example/src/app')
+  server.listen(8080)
+})
+
+test('/ route is loaded and responds', async t => {
   t.plan(1)
 
   const res = await request(server)
-    .get('/health')
+    .get('/')
+
+  t.is(res.status, 200)
+})
+
+test('/v1/inside route is loaded and responds', async t => {
+  t.plan(1)
+
+  const res = await request(server)
+    .get('/v1/inside')
 
   t.is(res.status, 200)
 })
